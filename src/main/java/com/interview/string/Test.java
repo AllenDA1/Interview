34a54cab1184452cda8d3ba8419dbdfd0d00c5b3
v-daijunj <v-daijunj.gd@chinatelecom.cn>
2023-03-22 16:31:48 +0800

    restart

--- src/main/java/com/interview/string/Test.java
+++ src/main/java/com/interview/string/Test.java
@@ -1,27 +1,27 @@
 package com.interview.string;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Test {
 
     public static void main(String[] args) {
-        String str = " He LL o W o r ld ! ";
-
-        String trim = str.trim();//去掉首尾空格
-        String replace = str.replace(" ", "");
-        String all = str.replaceAll(" ", "");
-
-        String s = str.replaceAll(" +", "");//去掉所有空格，包括首尾、中间
-
-
-        ArrayList<Object> list = new ArrayList<>();
-
-
-
-        System.out.println(trim);
-        System.out.println(replace);
-        System.out.println(all);
-
     }
+    
+    private void change(Boolean a){
+        a = true;
+    }
+    
+}
 
+
+//接口里的成员变量必须要实例化，可以使用static也可以普通实例化
+interface interTest {
+    public static final int a = 5;
+    int b = 0;
 }
